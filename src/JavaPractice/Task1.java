a6bc9e0d5c1036ec5592ae1d850f473da9b8a6ad
Jahanzeb2021 <jahanilyas3@gmail.com>
2022-08-20 16:10:20 -0700

    New Commit

---
+++ src/JavaPractice/Task1.java
@@ -0,0 +1,28 @@
+package JavaPractice;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		
+      int num = 100;
+      
+      while(num >= 1) {
+    	  if(num % 2==0) {
+    		  System.out.print(num+" ");
+    	  }
+    	  num--;
+      }
+      System.out.println();
+      System.out.println("--------------------------------");
+    int num1 = 100;
+      
+      while(num1 >= 1) {
+    	  if(num1 % 2==1) {
+    		  System.out.print(num1+" ");
+    	  }
+    	  num1--;
+      }
+      
+	}
+
+}
